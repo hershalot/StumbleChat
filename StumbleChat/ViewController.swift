ac8722d42c0d5d29009b8dda99750d795996fafa
Justin Hershey <fenapnu@justins-macbook-pro.local>
2017-02-13 10:33:48 -0500

    Initial Commit

---
+++ StumbleChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StumbleChat
+//
+//  Created by Justin Hershey on 2/13/17.
+//  Copyright © 2017 Fenapnu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
