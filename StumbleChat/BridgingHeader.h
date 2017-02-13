75bf143257f3c816fd0cec5cd4fb25eb9c7d974e
Justin Hershey <fenapnu@justins-macbook-pro.local>
2017-02-13 14:40:21 -0500

    Init Commit

---
+++ StumbleChat/BridgingHeader.h
@@ -0,0 +1,14 @@
+//
+//  BridgingHeader.h
+//  StumbleChat
+//
+//  Created by Justin Hershey on 2/13/17.
+//  Copyright © 2017 Fenapnu. All rights reserved.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#import "JSQMessagesViewController/JSQMessagesViewController.h"
+
+#endif /* BridgingHeader_h */
---
+++ StumbleChat/MessagingViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MessagingViewController.swift
+//  StumbleChat
+//
+//  Created by Justin Hershey on 2/13/17.
+//  Copyright © 2017 Fenapnu. All rights reserved.
+//
+
+import UIKit
+import JSQMessagesViewController
+
+
+
+
+class MessagingViewController: JSQMessagesViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
